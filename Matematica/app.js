0529327fe75532bedeb067c4eacdee1ea2797eb6
Andreia Gomes <gomesandreia27@outlook.com>
2018-05-25 10:44:03 -0300

    Exercicios Guiados

---
+++ Matematica/app.js
@@ -0,0 +1,10 @@
+var add = function(number1, number2){
+  return number1 + number2;
+};
+
+ console.log(add(9825, 777));
+
+ var multiply = function(number3){
+ return number3;
+ };
+ console.log(multiply(36325) * add(9825, 777));
